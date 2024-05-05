882914bffc200e9a9870fcde6360f18fdb211ca1
Ihor Mykhailychenko <igor.c.m@ukr.net>
2024-05-05 10:35:46 +0200

    feat: second task

--- main.py
+++ main.py
@@ -71,7 +71,23 @@ class Lexer:
                 self.advance()
                 return Token(TokenType.MINUS, "-")
 
-            raise LexicalError("Помилка лексичного аналізу")
+            if self.current_char == "*":
+                self.advance()
+                return Token(TokenType.MUL, "*")
+
+            if self.current_char == "/":
+                self.advance()
+                return Token(TokenType.DIV, "/")
+
+            if self.current_char == "(":
+                self.advance()
+                return Token(TokenType.LPAREN, "(")
+
+            if self.current_char == ")":
+                self.advance()
+                return Token(TokenType.RPAREN, ")")
+
+            raise LexicalError("Syntax error")
 
         return Token(TokenType.EOF, None)
 
@@ -113,9 +129,29 @@ class Parser:
 
     def term(self):
         """Парсер для 'term' правил граматики. У нашому випадку - це цілі числа."""
+        node = self.factor()
+        while self.current_token.type in (TokenType.MUL, TokenType.DIV):
+            token = self.current_token
+            if token.type == TokenType.MUL:
+                self.eat(TokenType.MUL)
+            elif token.type == TokenType.DIV:
+                self.eat(TokenType.DIV)
+
+            node = BinOp(left=node, op=token, right=self.factor())
+        return node
+
+    def factor(self):
         token = self.current_token
-        self.eat(TokenType.INTEGER)
-        return Num(token)
+        if token.type == TokenType.INTEGER:
+            self.eat(TokenType.INTEGER)
+            return Num(token)
+        elif token.type == TokenType.LPAREN:
+            self.eat(TokenType.LPAREN)
+            node = self.expr()
+            self.eat(TokenType.RPAREN)
+            return node
+        elif token.type == TokenType.RPAREN:
+            self.error()
 
     def expr(self):
         """Парсер для арифметичних виразів."""
@@ -157,6 +193,10 @@ class Interpreter:
             return self.visit(node.left) + self.visit(node.right)
         elif node.op.type == TokenType.MINUS:
             return self.visit(node.left) - self.visit(node.right)
+        elif node.op.type == TokenType.MUL:
+            return self.visit(node.left) * self.visit(node.right)
+        elif node.op.type == TokenType.DIV:
+            return self.visit(node.left) / self.visit(node.right)
 
     def visit_Num(self, node):
         return node.value
